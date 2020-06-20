0b5946e63bf36f042acde51c683390998613448f
Yohan <yohan@yohanpro.com>
2020-06-20 09:58:55 +0900

    Calendar 컴포넌트 등록

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,12 @@ import './index.css';
 import App from './components/App/App';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import Calendar from './components/Calendar';
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
+    <Calendar />
   </Provider>,
   document.getElementById('root')
 );
